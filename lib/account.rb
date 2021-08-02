ee564b5d4f25455ce70596d0f3fcd40493be51ce
Fabio <alessf13@gmail.com>
2021-08-02 15:36:02 +0300

    refactor: sum up @ammount to @balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,16 @@
 class Account
+	def initialize
+		@balance = 0
+		@transactions = []
+	end
 
 	def deposit(date, ammount)
 		@date = date
 		@ammount = ammount
+		@balance += @ammount
 	end
 
 	def print_bank_st
-		 "#{@date} || #{@ammount}.00 || || #{@ammount}.00"
+		 "#{@date} || #{@ammount}.00 || || #{@balance}.00"
 	end
 end
