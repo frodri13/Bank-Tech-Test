a1d3e29fe51a7971c7f4dc50908457112692c5c7
Fabio <alessf13@gmail.com>
2021-08-05 16:37:39 +0300

    refactor: instance doubles of transactions for account testing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,56 +1,43 @@
 require "account"
 
 RSpec.describe Account do
-  # it 'can take a deposit and print back a bank statement'do
-	# 	subject.deposit("10-01-2012", 1000)
-	# 	output = capture_stdout { subject.print_bank_st }
-	# 	expect(output.chomp).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00")
-	# end
-	# account = instance_double(Account, transactions: ["#{current_date} || 1000.00 || ||"], balance: 1000)
-	it 'can take a deposit and change the balance of the account' do
-		deposit = instance_double(Transaction, deposit: true, ammount: 1000)
-		subject.transaction(deposit)
-
-		expect(subject.transactions.first[0]).to be(deposit)
-		expect(subject.balance).to eq(1000)
-	end
-
-	it 'can take a withdraw and change the balance of the account' do
-		withdraw = instance_double(Transaction, deposit: false, ammount: 500)
-		
-		subject.transaction(withdraw)
-
-		expect(subject.transactions.first[0]).to be(withdraw)
-		expect(subject.balance).to eq(-500)
-	end
+	let(:deposit) {instance_double(Transaction, record: "#{current_date} || 1000.00 || ||", deposit: true, ammount: 1000)}
+	let(:deposit_1) {instance_double(Transaction, record: "10/01/2012 || 1000.00 || ||", deposit: true, ammount: 1000)}
+	let(:deposit_2) {instance_double(Transaction, record: "13/01/2012 || 2000.00 || ||", deposit: true, ammount: 2000)}
+	let(:withdraw) {instance_double(Transaction, record: "14/01/2012 || || 500.00 ||", deposit: false, ammount: 500)}
 
 	it 'can print a statement with one deposit' do
-		account = Account.new
-		deposit = Transaction.new(true, 1000)
-		account.transaction(deposit)
-		
-		expect(account.print_statement).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n")
+		subject.transaction(deposit_1)
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n")
 	end
 
 	it 'can print a statement with two deposits' do
-		account = Account.new
-		deposit_1 = Transaction.new(true, 1000)
-		deposit_2 = Transaction.new(true, 500)
-		account.transaction(deposit_1)
-		account.transaction(deposit_2)
+		subject.transaction(deposit_1)
+		subject.transaction(deposit_2)
 		
-		expect(account.print_statement).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n#{current_date} || 500.00 || || 1500.00\n")
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n13/01/2012 || 2000.00 || || 3000.00\n")
 	end
 
 	it 'can print a statement with two deposits and a withdraw' do
-		account = Account.new
-		deposit_1 = Transaction.new(true, 1000)
-		deposit_2 = Transaction.new(true, 2000)
-		withdraw = Transaction.new(false, 500)
-		account.transaction(deposit_1)
-		account.transaction(deposit_2)
-		account.transaction(withdraw)
+		subject.transaction(deposit_1)
+		subject.transaction(deposit_2)
+		subject.transaction(withdraw)
 
-		expect(account.print_statement).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n#{current_date} || 2000.00 || || 3000.00\n#{current_date} || || 500.00 || 2500.00\n")
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n13/01/2012 || 2000.00 || || 3000.00\n14/01/2012 || || 500.00 || 2500.00\n")
+	end
+
+		it 'can keep track of the date of the transaction'do
+		subject.transaction(deposit)
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n")
+	end
+
+	it 'prints the same output when asked multiple times'do
+		subject.transaction(deposit_1)
+		subject.transaction(deposit_2)
+		subject.transaction(withdraw)
+
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n13/01/2012 || 2000.00 || || 3000.00\n14/01/2012 || || 500.00 || 2500.00\n")
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n13/01/2012 || 2000.00 || || 3000.00\n14/01/2012 || || 500.00 || 2500.00\n")
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n13/01/2012 || 2000.00 || || 3000.00\n14/01/2012 || || 500.00 || 2500.00\n")
 	end
 end
