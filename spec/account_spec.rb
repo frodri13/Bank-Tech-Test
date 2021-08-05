26960cdb7dce555ad2651a2a61a8ca3ea3b5e43a
Fabio <alessf13@gmail.com>
2021-08-05 18:23:29 +0300

    test: add examples for negative results

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -26,7 +26,20 @@ RSpec.describe Account do
 		expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00\n13/01/2012 || 2000.00 || || 3000.00\n14/01/2012 || || 500.00 || 2500.00\n")
 	end
 
-		it 'can keep track of the date of the transaction'do
+	it 'can print out a negative balance' do
+		subject.add_transaction(withdraw)
+
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n14/01/2012 || || 500.00 || -500.00\n")
+	end
+
+	it 'can add up multiple withdrawals' do
+		subject.add_transaction(withdraw)
+		subject.add_transaction(withdraw)
+
+		expect(subject.print_statement).to eq("date || credit || debit || balance\n14/01/2012 || || 500.00 || -500.00\n14/01/2012 || || 500.00 || -1000.00\n")
+	end
+
+	it 'can keep track of the date of the transaction'do
 		subject.add_transaction(deposit)
 		expect(subject.print_statement).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n")
 	end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,6 +4,7 @@ require 'transaction'
 RSpec.describe Statement do
 	let(:deposit_1) { instance_double(Transaction, record: "#{current_date} || 1000.00 || ||", deposit: true, ammount: 1000)}
 	let(:deposit_2) { instance_double(Transaction, record: "#{current_date} || 2000.00 || ||", deposit: true, ammount: 2000)}
+	let(:withdraw) {instance_double(Transaction, record: "#{current_date} || || 500.00 ||", deposit: false, ammount: 500)}
 
 	it 'can print a stament of a deposit'do
 		balance_1 = 1000
@@ -18,5 +19,29 @@ RSpec.describe Statement do
 
 		statement = Statement.new([[deposit_1, balance_1], [deposit_2, balance_2]])
 		expect(statement.print).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n#{current_date} || 2000.00 || || 3000.00\n")
+	end 
+
+	it 'can print a statement with only one withdraw' do
+		negative_balance = -500
+		statement = Statement.new([[withdraw, negative_balance]])
+
+		expect(statement.print).to eq("date || credit || debit || balance\n#{current_date} || || 500.00 || -500.00\n")
+	end
+
+	it 'can print a statement with multiple withdraws' do
+		negative_balance_1 = -500
+		negative_balance_2 = -1000
+		statement = Statement.new([[withdraw, negative_balance_1], [withdraw, negative_balance_2]])
+
+		expect(statement.print).to eq("date || credit || debit || balance\n#{current_date} || || 500.00 || -500.00\n#{current_date} || || 500.00 || -1000.00\n")
+	end
+
+	it 'can print a stament of multiple deposits and one withdraw'do
+		balance_1 = 1000
+		balance_2 = 3000
+		balance_3 = 2500
+ 
+		statement = Statement.new([[deposit_1, balance_1], [deposit_2, balance_2], [withdraw, balance_3]])
+		expect(statement.print).to eq("date || credit || debit || balance\n#{current_date} || 1000.00 || || 1000.00\n#{current_date} || 2000.00 || || 3000.00\n#{current_date} || || 500.00 || 2500.00\n")
 	end
 end
